4364ad3187eeb3ecd1c950ba179a77116f2e3b4f
Ayush Ranjan <ankitsingh76857@gmail.com>
2026-08-05 05:15:19 +0530

    Time: 12 ms (6.56%), Space: 48.7 MB (5.6%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -6,23 +6,51 @@ class Solution {
         int n = graph.length;
 
         outer = new ArrayList<>();
+        //     ArrayList<Integer> inner = new ArrayList<>();
+        //     inner.add(0);
+        //     dfs(inner, 0, graph);
+        //     return outer;
+
+        // }
+
+        // void dfs(ArrayList<Integer> inner, int u, int[][] graph) {
+        //     if (u == graph.length - 1) {
+        //         outer.add(new ArrayList<>(inner));
+        //         return;
+        //     }
+
+        //     for (int v : graph[u]) {
+        //         inner.add(v);
+        //         dfs(inner, v, graph);
+        //         inner.remove(inner.size() - 1);
+        //     }
+
+        Queue<List<Integer>> q = new LinkedList<>();
+
         ArrayList<Integer> inner = new ArrayList<>();
+
         inner.add(0);
-        dfs(inner, 0, graph);
+
+        q.offer(inner);
+
+        while (!q.isEmpty()) {
+            List<Integer> path = q.poll();
+
+            int u = path.get(path.size() - 1);
+
+            if (u == n - 1){
+                outer.add(path);
+                continue;
+            }
+
+            for (int next : graph[u]) {
+                List<Integer> newPath = new ArrayList<>(path);
+                newPath.add(next);
+                q.offer(newPath);
+            }
+        }
+
         return outer;
-
     }
 
-    void dfs(ArrayList<Integer> inner, int u, int[][] graph) {
-        if (u == graph.length - 1) {
-            outer.add(new ArrayList<>(inner));
-            return;
-        }
-
-        for (int v : graph[u]) {
-            inner.add(v);
-            dfs(inner, v, graph);
-            inner.remove(inner.size() - 1);
-        }
-    }
 }
